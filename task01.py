9433df70376e472b2cf5539de1fa92c23ea2d107
kosmicheskiy <bezvoshchuk.v@gmail.com>
2024-05-26 17:38:49 +0300

    Update task01.py

--- task01.py
+++ task01.py
@@ -1,3 +1,5 @@
+import timeit
+
 def find_coins_greedy(amount):
     denominations = [50, 20, 10, 5, 2, 1]  # доступні номінали монет
     coins_used = {}  # словник для зберігання кількості монет кожного номіналу
@@ -11,7 +13,23 @@ def find_coins_greedy(amount):
     return coins_used
 
 # Приклад використання
-amount = 113
-result = find_coins_greedy(amount)
+result = find_coins_greedy(113)
 print(result)
+result = find_coins_greedy(5003)
+print(result)
+result = find_coins_greedy(150958)
+print(result)
+
+print("Calculation of time used by find_coins_greedy")
+print(timeit.timeit(lambda: find_coins_greedy(113), number=10))
+print(timeit.timeit(lambda: find_coins_greedy(5003), number=10))
+print(timeit.timeit(lambda: find_coins_greedy(150958), number=10))
+
+
 #{50: 2, 10: 1, 2: 1, 1: 1}
+#{50: 100, 2: 1, 1: 1}
+#{50: 3019, 5: 1, 2: 1, 1: 1}
+#Calculation of time used by find_coins_greedy
+#1.2884000170743093e-05
+#1.0990999726345763e-05
+#1.2559999959194101e-05
